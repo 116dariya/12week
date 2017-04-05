78cc6607a142d829806ae809fe16d6e9ed91967b
116dariya <116dariya@gmail.com>
2017-04-05 11:48:17 +0600

    Add files via upload
    Факториал

--- 666.py
+++ 666.py
@@ -1,19 +1,29 @@
 from multiprocessing import Pool
 
-def summ(t):
-	k , l = t
-	s = 0
-	for i in range(k , l + 1 ):
-		s = s + i
+
+def f(j):
+	q , w = j
+	s = 1
+	for item in range(q , w + 1):
+		
+		s = item * s
 	return s
 
+
 if __name__ == '__main__':
-    pool = Pool(processes=2)
-    n = 100
-    l = pool.map(summ, [(0, n//2),(((n//2)+1),n)])
+    pool = Pool(processes=4)
+    n = 5
+    l = pool.map(f, [(1, n // 2), ( n// 2 + 1, n )])
     print(l)
-    s = 0
+    m = 1
     for i in l:
-    	s = s + i
-    print(s)
+    	m = m * i
+    print(m)
     
+    # i = 2
+    # for item in l:
+    # 	if item == True:
+    # 		print(i)
+    # 	i = i + 1
+    #print(l)
+    #print(primes)
